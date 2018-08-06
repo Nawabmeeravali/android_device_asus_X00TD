1f2e12e1a7ee4da451d2092c6200fdb8750b208a
Nawabmeeravali <meeravalinawab7@gmail.com>
2018-08-06 21:38:12 +0530

    aoscp ready

--- device.mk
+++ device.mk
@@ -344,13 +344,6 @@ PRODUCT_PACKAGES += \
     libstagefright_wfd \
     libstagefrighthw
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
